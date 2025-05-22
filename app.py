1149fc7a6ee831b2d3ce82e42545e4c87ab01306
Yousfi Mirhame <mirhameyousfi@gmail.com>
2025-05-22 19:54:32 +0200

    Fix Render port issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
